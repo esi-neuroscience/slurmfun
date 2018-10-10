1312f173c18c2efc91a92f3cf8eb9ff4f5a2ea8d
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2018-10-10 14:57:30 +0200

    Minor changes to test_slurmfun

--- test/myfunction.m
+++ test/myfunction.m
@@ -2,7 +2,7 @@ function out = myfunction(in1, in2)
 
 fprintf('Creating %g random numbers\n', in1)
 out = rand(in1,in2);
-tWait = randi(10)+40;
+tWait = randi(10)+20;
 fprintf('Pausing for %g s\n', tWait)
 pause(tWait);
 
--- test/test_slurmfun.m
+++ test/test_slurmfun.m
@@ -2,7 +2,7 @@
 clc
 % addpath /opt/ESIsoftware/slurmfun
 dbstop if error
-nJobs = 10;
+nJobs = 200;
 inputArgs1 = num2cell(randi(20,nJobs,1)+60);
 inputArgs2 = num2cell(randi(20,nJobs,1)+60);
 inputArgs1{end+1} = 5000000000;
@@ -22,7 +22,7 @@ inputArgs2{end+1} = 1;
     'stopOnError', false, ...
     'deleteFiles', false, ...
     'waitForToolboxes', {}, ...
-    'waitForReturn', false);
+    'waitForReturn', true);
 
 
 
