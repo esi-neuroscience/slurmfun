8c952c8a0b8248061b31830a475304c9a03ad041
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2018-10-17 09:44:33 +0200

    FIX: logfiles weren't deleted at end of slurmfun

--- slurmfun.m
+++ slurmfun.m
@@ -235,7 +235,7 @@ fprintf('Submission of %u jobs took %.0f s\n', nJobs, toc(tSubmission))
 
 % Setup cleanup after completion/failure
 if parser.Results.deleteFiles && parser.Results.waitForReturn
-    cleanup = onCleanup(@() delete_if_exist([{jobs.inputFile}, {jobs.outputFile}], ...
+    cleanup = onCleanup(@() delete_if_exist([{jobs.inputFile}, {jobs.outputFile}, {jobs([jobs.deleteFiles]).logFile}], ...
         parser.Results.slurmWorkingDirectory, slurmWDCreated, LD_PRELOAD));
 end
 
