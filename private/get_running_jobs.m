34449def4bccb23a04fe31d7e1ba020c0661b4c2
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2019-05-29 15:45:40 +0200

    Added retry for squeue query until timeout
    squeue queries can sometimes fail. We now try again for maximally
    5 s at 0.5 s intervals.
    
     Changes to be committed:
    	modified:   private/get_running_jobs.m

--- private/get_running_jobs.m
+++ private/get_running_jobs.m
@@ -11,8 +11,18 @@ end
 tmpFile = fullfile('/tmp/', sprintf('%s_jobs_%u', account, pid));
 squeueCmd = sprintf('squeue -A %s -h -o "%%A %%T">%s', account, tmpFile);
 
-[result, out] = system_out_to_disk(squeueCmd);
-assert(result == 0, 'squeue query failed');
+tStart = tic;
+timeout = 5;
+result = -1;
+
+while (toc(tStart) < timeout)
+    [result, out] = system_out_to_disk(squeueCmd);
+    if result == 0
+        break
+    end
+    pause(0.5)    
+end        
+assert(result == 0, 'squeue query failed: %s', out);
 
 if ~isempty(out)
     out = textscan(out, '%f%s');
