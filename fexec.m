f881e4914102d9aeab9c5a60bce02d23de87ba41
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2020-04-29 10:46:21 +0200

    NEW: added varargout to fexec
    To allow for multiple output arguments fexec now uses varargout.
    This still needs to be tested and implemented on the slurmfun
    level.

--- fexec.m
+++ fexec.m
@@ -3,21 +3,16 @@ function out = fexec(func, inputVars, outputFile)
 % input file must contain the variables func, inputVars, outputFile
 fprintf('Trying to evaluate %s\n', func2str(func))
 try
-    switch nargout(func)
-        case 1            
-            out = feval(func, inputVars{:});
-        case 0
-            feval(func, inputVars{:});
-            out = 'no output';
-            
-    end
+    varargout = feval(func, inputVars{:});
+    out = varargout;
     outSize = whos('out');
     if outSize.bytes > 2*1024*1024*1024
         error(['Size of the output arguments must not exceed 2 GB. ', ...
             'For large data please save to disk in your function'])
     end
+    
 catch me
-    display(me)
+    me.display();
     out = me;
     
 end
