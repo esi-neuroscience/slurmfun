ea8b233f3e1b534322acafd605196243b65e328d
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2020-04-30 10:18:21 +0200

    FIX: bug in validate_partition

--- private/validate_partition.m
+++ private/validate_partition.m
@@ -2,11 +2,15 @@ function isValid = validate_partition(partition)
 
 availablePartitions = get_available_partitions();
 
-is_partition = @(x) ischar(validatestring(x, availablePartitions));
+is_partition = @(x) ismember(x, availablePartitions);
 
 if iscell(partition)
     isValid = all(cellfun(is_partition, partition));
-else
+elseif ischar(partition)
     isValid = is_partition(partition);
+else
+    error('partition must be a string or cell of string')
 end
+
+
     
