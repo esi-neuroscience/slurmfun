79f31d04ec344d8e6bc7585a1db84b78f2c53de4
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2020-04-30 10:17:32 +0200

    NEW: added doc string

--- private/system_out_to_disk.m
+++ private/system_out_to_disk.m
@@ -1,4 +1,13 @@
 function [result, out] = system_out_to_disk(cmd)
+% SYSTEM_OUT_TO_DISK - Execute system command and return result.
+%
+%   [status, result] = system_out_to_disk('command')
+% 
+% In rare cases on Linux, using system may not return the complete buffer.
+% This function is a workaround that pipes the output into a file and reads
+% it back in.
+% 
+% See also system
 persistent pid
 if isempty(pid)
     pid = feature('getpid');
