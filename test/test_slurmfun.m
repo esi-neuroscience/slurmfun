d0d5fd4dd33a5f68cb8107f68df7f71d3a232738
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2025-03-31 11:39:29 +0200

    CHG: Updated tests
    - include more output to improve legibility
    
    On branch dev
    Changes to be committed:
    	modified:   test/test_slurmfun.m

--- test/test_slurmfun.m
+++ test/test_slurmfun.m
@@ -2,6 +2,10 @@ addpath(fileparts(fileparts(mfilename('fullpath'))))
 
 clc
 
+delim = repmat(['-'], 1, 80);
+testStart = datetime('now');
+fprintf('<strong>%s\n\t\t Test Session Starts at %s\n%s</strong>\n', delim, testStart, delim);
+
 % prepare defaults based on cluster
 machine = getenv('HOSTNAME');
 if contains(machine, 'bic-svhpc')
@@ -19,6 +23,7 @@ end
 dbstop if error
 
 %% Start `nJobs` in `defaultPartition` and allocate ~2.3 GB array
+fprintf('\t Test array allocation in %s...\n', defaultPartition)
 nJobs = 5;
 inputArgs1 = num2cell(randi(20,nJobs,1)+60);
 inputArgs2 = num2cell(randi(20,nJobs,1)+60);
@@ -33,8 +38,10 @@ inputArgs2{end+1} = 1;
     'cpu', 1, ...
     'waitForReturn', true);
 assert(numel(out) == nJobs + 1)
+fprintf('\t Passed\n%s\n', delim)
 
 %% test varying partitions
+fprintf('\t Test multiple partitions/CPU/mem specs...\n')
 mem = {'7500M', '15500M', '7500'};
 cpu = [1, 1, 4];
 nJobs = 3;
@@ -53,24 +60,30 @@ for i = 1:3
     assert(size(out{i}, 1) == inputArgs1{i})
     assert(size(out{i}, 2) == inputArgs2{i})
 end
+fprintf('\t Passed\n%s\n', delim)
 
 %% no outputs
+fprintf('\t Test function with no outputs...\n')
 [out, jobs] = slurmfun(@function_without_output, {'in1'}, {'in2'}, ...
     'partition', defaultPartition, ...
     'stopOnError', false, ...
     'deleteFiles', true, ...    
     'waitForReturn', true);
 assert(strcmp(out{1}, 'no output'))
+fprintf('\t Passed\n%s\n', delim)
 
 %% multiple outputs
+fprintf('\t Test function with multiple outputs...\n')
 [out, jobs] = slurmfun(@function_with_multiple_outputs, {'in1'}, {'in2'}, ...
     'partition', defaultPartition, ...
     'stopOnError', false, ...
     'deleteFiles', true, ...    
     'waitForReturn', true);
 assert(numel(out{1}) == 2);
+fprintf('\t Passed\n%s\n', delim)
 
 %% handle errors in user functions
+fprintf('\t Test error handling...\n')
 expectedError = 'Unrecognized function or variable ''jgldjdfgl''.';
 try
      [out, jobs] = slurmfun(@myfunction_with_errors, inputArgs1(1), ...
@@ -88,4 +101,11 @@ end
                        'deleteFiles', false, ...
                        'waitForToolboxes', {}, ...
                        'waitForReturn', true);
-assert(contains(out.message, expectedError));
+assert(contains(out{1}.message, expectedError));
+fprintf('\t Passed\n%s\n', delim)
+
+%% end of test session
+testEnd = datetime('now');
+testDur = testEnd - testStart;
+testDur.format = 'mm:ss';
+fprintf('<strong>\t\t ALL PASSED in %s\n%s</strong>\n', testDur, delim)
